9711ee336d5ff50fd84173eafbd42dab989a97d4
Xander May <xandermay1@gmail.com>
2021-08-01 11:20:57 -0400

    Update models.vue

--- pages/models.vue
+++ pages/models.vue
@@ -14,6 +14,30 @@
         >
           <div class="grid grid-cols-4 gap-4">
             <div class="bg-gray-100 rounded-md">
+             <model-viewer src="/model_files/animated_astronaut.glb" camera-controls exposure="0.72" shadow-intensity="2.5" autoplay auto-rotate>
+            </model-viewer>
+            </div>
+            <div class="col-span-3">
+              <div class="pt-6 space-y-4">
+                <blockquote>
+                  <p class="text-lg font-semibold">
+                    "To Infinity, and Beyond!"
+                  </p>
+                </blockquote>
+                <figcaption class="font-medium">
+                  <div class="text-cyan-600">This actually has an animation. I think theres a bug in the model-viewer package thats not playing it because it works in a lot of other GLTF viewers. If you want to see it, download the file and go to https://gltf-viewer.donmccurdy.com/</div>
+                  <div class="text-cyan-600">Xander May</div>
+                  <div cla> 08/01/2021 </div>
+                </figcaption>
+              </div>
+            </div>
+          </div>
+        </figure>
+      <figure
+          class="blogpost blogfig container mx-auto rounded-xl p-5 m-4 l-4"
+        >
+          <div class="grid grid-cols-4 gap-4">
+            <div class="bg-gray-100 rounded-md">
              <model-viewer src="/model_files/odst_helmet.glb" auto-rotate camera-orbit="0.7684deg 89.6deg 0.333m" min-camera-orbit="auto auto auto" max-camera-orbit="auto auto 0.333m">
              </model-viewer>
             </div>
