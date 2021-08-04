4a359a2b4b73f9237e2d54aa8e0a5f6bc8b46052
Xander May <xandermay1@gmail.com>
2021-08-04 06:52:49 -0400

    Added todays model

--- pages/models.vue
+++ pages/models.vue
@@ -8,7 +8,29 @@
     <div>
       <the-header></the-header>
       <header class="bg-white shadow"></header>
-      <main>
+      <main><figure
+          class="blogpost blogfig container mx-auto rounded-xl p-5 m-4 l-4"
+        >
+          <div class="grid grid-cols-4 gap-4">
+            <div class="model">
+              <model-viewer src="/model_files/magic_8_ball.glb" camera-controls exposure="1" shadow-intensity="4.4" autoplay auto-rotate />
+            </div>
+            <div class="col-span-3">
+              <div class="pt-6 space-y-4">
+                <blockquote>
+                  <p class="text-lg font-semibold">"Will I stay motivated to do these forever?"</p>
+                </blockquote>
+                <div class="text-cyan-600">
+                  Simple one today, yesterdays took way too much time. I chose to do a boolean on the text to avoid learning proper texturing :)
+                </div>
+                <figcaption class="font-medium">
+                  <div class="text-cyan-600">Xander May</div>
+                  <div cla>08/05/2021</div>
+                </figcaption>
+              </div>
+            </div>
+          </div>
+        </figure>
         <figure
           class="blogpost blogfig container mx-auto rounded-xl p-5 m-4 l-4"
         >
